2dc128646da05f54984cb091b3ca4f86d3efce63
Pema Singi Tamang <waibapemasingi2055@gmail.com>
2024-02-22 09:20:21 +0545

    changes in operator.py to handle invalid option by user

--- Operator.py
+++ Operator.py
@@ -22,49 +22,57 @@ class Mul(Sub):
 class Div(Mul):
     pass
     def division(self):
-      return self.number1/self.number2 #Divison4
+        try:
+         return self.number1/self.number2 #Divison4
+        except ZeroDivisionError:
+            exit("you can not divide by zero to any number sir")
+
+def user_input():
+    n1 = int(input("Enter first number:_"))  # Input first number
+    n2 = int(input("Enter the second number:_"))  # Input second number
+    return n1, n2
 
 while True:
     print("What do you want?")
     option_input=("1.Addition\n 2.Subtract\n 3.Multiply \n 4.Divide ")
     print(option_input)
-
     try:
      select_number=int(input("Select the above option:_"))
     except ValueError:
              exit("Give only integer value")
-
-    n1 = int(input("Enter first number:_"))  # Input first number
-    n2 = int(input("Enter the second number:_"))  # Input second number
-
-    # object created
-    a = Add(n1, n2)  # Parameter pass
-    s = Sub(n1, n2)
-    m = Mul(n1, n2)
-    d = Div(n1, n2)
-
     if select_number == 1:
-
-            print("Result is:", a.addition())
-
-
+        x, y = user_input()
+        a = Add(x, y)
+        print("Result is:", a.addition())
     elif select_number == 2:
-            print("The result is :", s.subtraction())
-
-
+        x, y = user_input()
+        s = Sub(x, y)
+        print("The result is :", s.subtraction())
     elif select_number == 3:
-            print("The result is:", m.multiplication())
+        x, y = user_input()
+        m = Mul(x, y)
+        print("The result is:", m.multiplication())
     elif select_number == 4:
-            print("The result is:", d.division())
+        x, y = user_input()
+        d = Div(x, y)
+        print("The result is:", d.division())
     else:
-            print("Invalid option")
-            break
+        print("Invalid option")
+        break
+def exit_method():
+    yesno = input("Do you want continue[y or n]:_")
+    if yesno == 'y' or yesno == 'Y':
+        c.calculation()
+        exit_method()  # loop
+    else:
+        print("Exit program")
+
 # Calculator 2: Using class , function , object
 class Cal:
     def calculation(self):
+      operator_choose = input("choose operator:['+','-','*','/']_")
       self.num1 = int(input("Enter number 1:_"))
       self.num2 = int(input("Enter number 2:_"))
-      operator_choose = input("choose operator:['+','-','*','/']_")
       if operator_choose == '+':
          return print("The sum is:",self.num1+self.num2)
       elif operator_choose == '-':
