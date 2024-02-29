3844d0e0f83c46ce59d2f0b648a1610aa5fa8698
Pema Singi Tamang <waibapemasingi2055@gmail.com>
2024-02-29 22:04:07 +0545

    Add pandas format

--- Pandas.py
+++ Pandas.py
@@ -1,20 +1,10 @@
-import numpy as np
 import pandas as pd
-detail_list = []
-number = int(input("How many times do want add information:_"))
-for i in range(number):
-    Id = int(input("Enter the id:_"))
-    Name = input("Enter the name:_")
-    Address = input("Enter the address:_")
-    Faculty = input("Enter the faculty:_")
-    detail_list.append({
-        "Id": Id,
-        "Name": Name,
-        "Address": Address,
-        "Faculty": Faculty
-            })
-    print(detail_list)
-df = pd.DataFrame(detail_list)
+dict1 = {
+    "Id": [1, 2, 3],
+    "Name": ['Pema', 'Ram', 'Sita'],
+    "Address": ['sindhu', 'Usa', 'kathmandu'],
+    "Faculty": ['Management', 'Science', 'Education']
+}
+
+df = pd.DataFrame(dict1)
 df.head()
-
-
